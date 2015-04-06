1e6aa5926860871e7034927d9b2b05c4f3a1492f
Andrew Du <aadu@ucsd.edu>
2015-04-06 14:13:24 -0700

    fixed spacing

--- Design/LRUCache.java
+++ Design/LRUCache.java
@@ -1,6 +1,7 @@
 /* Leetcode
  * get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
-set(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before 
+ * set(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity,
+ *	 it should invalidate the least recently used item before 
  */
 public class LRUCache {
 	private class DNode{
