3bc043fbe7c225fb5ee65d539fc29f4af2a76938
Andrew Du <aadu@ucsd.edu>
2015-04-06 00:34:33 -0700

    change is inevitable

--- Strings/TwoAreAnagrams.java
+++ Strings/TwoAreAnagrams.java
@@ -3,6 +3,23 @@
  * 	anagrams("abc", "cba") -> true
  *	anagrams("abcd", "abc") -> false
  */
+
+// Time: O(n)
+// Space: O(1) - ascii,  O(n) - any character
+public static boolean anagrams(String a, String b){
+	int[] map = new int[256];
+	for(int i=0; i<a.length(); i++){
+		map[a.charAt(i)]++;
+	}
+	for(int i=0; i<b.length(); i++){
+		map[b.charAt(i)]--;
+		if(map[b.charAt(i)] < 0) return false;
+	}
+	return true;
+}
+
+// Time: O(nlogn)
+// Space: O(n) or O(1) - if params char[]
 public boolean anagrams(String a, String b){
 	if(a.length() != b.length())
 		return false;
