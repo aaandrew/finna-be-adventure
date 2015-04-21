1c1cef93c842e066b51081a2a9552b1298452c17
Andrew Du <aadu@ucsd.edu>
2015-04-21 02:13:27 -0700

    she workin with the spatula, I know she love flipping shit

---
+++ Design/MinStack.java
@@ -0,0 +1,34 @@
+/* Leetcode
+ * Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+ * push(x) -- Push element x onto stack.
+ * pop() -- Removes the element on top of the stack.
+ * top() -- Get the top element.
+ * getMin() -- Retrieve the minimum element in the stack.
+ */
+
+class MinStack {
+	Stack<Integer> stack = new Stack<Integer>();
+	Stack<Integer> minStack = new Stack<Integer>();
+
+	public void push(int x) {
+		stack.push(x);
+		if(minStack.empty() || minStack.peek() >= x){
+			minStack.push(x);
+		}
+	}
+
+	public void pop() {
+		int x = stack.pop();
+		if(x == minStack.peek()){
+			minStack.pop();
+		}
+	}
+
+	public int top() {
+		return stack.peek();
+	}
+
+	public int getMin() {
+		return minStack.peek();
+	}
+}
