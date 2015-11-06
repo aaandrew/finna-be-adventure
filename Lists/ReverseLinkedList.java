2f0498f1a50905b27c892843549d5d0f64f41bf6
Andrew Du <aadu@ucsd.edu>
2015-11-05 16:03:28 -0800

    better name

--- Lists/ReverseLinkedList.java
+++ Lists/ReverseLinkedList.java
@@ -36,12 +36,12 @@ public ListNode reverseListHelper(ListNode prev, ListNode curr){
 public ListNode reverseList(ListNode head) {
 	if(head == null || head.next == null)
 		return head;
-	ListNode newHead = null, next = null;
+	ListNode prev = null, next = null;
 	while(head != null){
 		next = head.next;
-		head.next = newHead;
-		newHead = head;
+		head.next = prev;
+		prev = head;
 		head = next;
 	}
-	return newHead;
+	return prev;
 }
