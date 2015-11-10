0d512af4f058bc8316fd8380e8f69eeaadd606c2
Andrew Du <aadu@ucsd.edu>
2015-11-09 23:52:53 -0800

    pramp

--- Schedules/FirstAvailableTime.java
+++ Schedules/FirstAvailableTime.java
@@ -1,44 +1,37 @@
 // PRAMP
-// This code is in js, convert to java later
-
-A = [[4,8],[1,3],[9,10],[]]
-B = [[],[],[],[]]
+// TODO: Write explanation + examples
+// {10,11}, {5,9},{1,4}
+// {1,3},{10,20},{6,9}
 
 /* 
  * Given two time intervals and a meeting duration time,
  * find the first time both A and B are available. 
  */
-public static int[] getMinDuration(int[][] timeA, int[][] timeB, int duration){
-   // Sort both time schedules
+public static int[] firstTimeAvailable(int[][] timeA, int[][] timeB, int duration){
    Comparator<int[]> comparator = new Comparator<int[]>(){
-     @Override
-     public int compare(final int[] a, final int[] b){
+      @Override
+      public int compare(int[] a, int[] b){
       // Compare the end times
-      return a[1] - b[1];
+         return a[1] -b[1];
+      }
    };
-
+   // Sort both arrays
    Arrays.sort(timeA, comparator);
    Arrays.sort(timeB, comparator);
-
-
-}
-
-function getMinDuration(timeA, timeB, duration){
-   sort(timeA, comparator); sort(timeB, comparator);
-   
-   var a = 0; 
-   var b = 0;
-   
+   int a = 0;
+   int b = 0;
    while(a < timeA.length && b < timeB.length){
-      var maxStartTime = Math.max(timeA[a][0], timeB[b][0]);
-      var minEndTime = Math.min(timeA[a][1], timeB[b][1]);
-      if(minEndTime - maxStartTime + 1 >= duration){
-         return [maxStartTime, maxStartTime + duration];
+      // Check if the overlap of the times fits within the duration
+      int maxStartTime = Math.max(timeA[a][0], timeB[b][0]);
+      int minEndTime = Math.min(timeA[a][1], timeB[b][1]);
+      if(maxStartTime + duration <= minEndTime){
+         return new int[]{maxStartTime, maxStartTime + duration};
+      // Increment pointer with least end time
       }else if(timeA[a][1] < timeB[b][1]){
          a++;
       }else{
          b++;
       }
    }
-   return [];
+   return new int[]{};
 }
